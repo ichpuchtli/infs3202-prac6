329cb25e4634aaeaec6c3e7aba734907483a4b48
Sam Macpherson <sam.mack91@gmail.com>
2015-05-31 21:52:49 +1000

    swap task 2 with task 1 as index.php

--- index.php
+++ index.php
@@ -2,7 +2,17 @@
 	
 date_default_timezone_set("Australia/Brisbane");
 
-$hits = unserialize(file_get_contents("cache_file"));
+$host = "au-cdbr-azure-east-a.cloudapp.net";
+$user = "b710479716e9e4";
+$pwd = "4ed8c2dd";
+$db = "hits2";
+
+$conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
+$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+$sql = 'select counter from hits limit 1';
+
+$hits = $conn->query($sql)->fetchColumn();;
 
 if($hits == false){
 	$hits = 0;
@@ -10,8 +20,9 @@ if($hits == false){
 
 $hits += 1;
 
+$sql = 'update hits set counter=' . $hits;
 
-file_put_contents("cache_file", serialize($hits));
+$conn->query($sql);
 
 ?>
 
@@ -21,6 +32,8 @@ file_put_contents("cache_file", serialize($hits));
 	<title>INFS3202-Prac6</title>
 </head>
 <body style="margin: 10% 0 auto 0; text-align: center;">
+	<h4>Task 2</h4>
+	<br>
 	<h1>Total Guests: <?php echo $hits; ?></h1>
 
 	<h3> This page will auto refresh every 2 seconds </h3>
--- task1.php
+++ task1.php
@@ -2,17 +2,7 @@
 	
 date_default_timezone_set("Australia/Brisbane");
 
-$host = "au-cdbr-azure-east-a.cloudapp.net";
-$user = "b710479716e9e4";
-$pwd = "4ed8c2dd";
-$db = "hits2";
-
-$conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
-$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-
-$sql = 'select counter from hits limit 1';
-
-$hits = $conn->query($sql)->fetchColumn();;
+$hits = unserialize(file_get_contents("cache_file"));
 
 if($hits == false){
 	$hits = 0;
@@ -20,9 +10,8 @@ if($hits == false){
 
 $hits += 1;
 
-$sql = 'update hits set counter=' . $hits;
 
-$conn->query($sql);
+file_put_contents("cache_file", serialize($hits));
 
 ?>
 
@@ -32,8 +21,6 @@ $conn->query($sql);
 	<title>INFS3202-Prac6</title>
 </head>
 <body style="margin: 10% 0 auto 0; text-align: center;">
-	<h4>Task 2</h4>
-	<br>
 	<h1>Total Guests: <?php echo $hits; ?></h1>
 
 	<h3> This page will auto refresh every 2 seconds </h3>
